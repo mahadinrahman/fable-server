b2f99aa96076481d7fe8415f5b4d4ea0cd2a0a33
mahadinrahman <mahadinrahman333@gmail.com>
2026-06-25 23:14:21 +0600

    api

--- index.js
+++ index.js
@@ -155,6 +155,13 @@ async function run() {
       res.send(result);
     })
 
+    // user API...........................
+
+    app.get('/users', async (req, res) => {
+      const result =await usersCollection.find().toArray();
+      res.send(result);
+    })
+
 
 
     // Send a ping to confirm a successful connection
